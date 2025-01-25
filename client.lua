ec8e1920711234a1bd8f7b363d3b06196b4f1367
PandaScriptsFivem <pandascripts.hun@gmail.com>
2025-01-25 07:44:47 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -2,66 +2,118 @@ local noclipActive = false
 local playerPed = PlayerPedId()
 
 local function startParachuteAnimation()
-    RequestAnimDict("skydive@base")
-    while not HasAnimDictLoaded("skydive@base") do
-        Wait(10)
-    end
-    TaskPlayAnim(playerPed, "skydive@base", "free_idle", 8.0, 8.0, -1, 3, 0, false, false, false)
+    lib.playAnim(playerPed, "skydive@base", "free_idle", 8.0, 8.0, -1, 3)
 end
 
 local function stopParachuteAnimation()
     ClearPedTasks(playerPed)
 end
 
+local function placeOnGround()
+    local entity = playerPed
+    if IsPedInAnyVehicle(playerPed, false) then
+        entity = GetVehiclePedIsIn(playerPed, false)
+    end
+
+    local pos = GetEntityCoords(entity)
+    local foundGround, groundZ = false, nil
+    local zCoord = pos.z
+
+    for i = 0, 1000, 1 do
+        foundGround, groundZ = GetGroundZFor_3dCoord(pos.x, pos.y, zCoord - i, false)
+        if foundGround then
+            break
+        end
+    end
+
+    if foundGround then
+        SetEntityCoords(entity, pos.x, pos.y, groundZ, true, true, true)
+    else
+        lib.notify({ title = "Nem sikerült földet találni", type = "error" })
+    end
+end
+
 local function toggleNoclip()
     noclipActive = not noclipActive
-    local playerPed = PlayerPedId()
+    playerPed = PlayerPedId()
 
     if noclipActive then
-        startParachuteAnimation()
-        SetEntityInvincible(playerPed, true)
-        FreezeEntityPosition(playerPed, true)
+        if IsPedInAnyVehicle(playerPed, false) then
+            local vehicle = GetVehiclePedIsIn(playerPed, false)
+            FreezeEntityPosition(vehicle, true)
+        else
+            startParachuteAnimation()
+            FreezeEntityPosition(playerPed, true)
+        end
+        SetLocalPlayerAsGhost(true)
+        SetGhostedEntityAlpha(0)
+        NetworkSetPlayerIsPassive(true)
         lib.notify({ title = "Noclip aktiválva", type = "success" })
     else
-        stopParachuteAnimation()
-        SetEntityInvincible(playerPed, false)
-        FreezeEntityPosition(playerPed, false)
+        if IsPedInAnyVehicle(playerPed, false) then
+            local vehicle = GetVehiclePedIsIn(playerPed, false)
+            FreezeEntityPosition(vehicle, false)
+        else
+            stopParachuteAnimation()
+            FreezeEntityPosition(playerPed, false)
+        end
+        placeOnGround()
+        SetLocalPlayerAsGhost(false)
+        NetworkSetPlayerIsPassive(false)
         lib.notify({ title = "Noclip kikapcsolva", type = "error" })
     end
 end
 
 local function handleNoclipMovement()
-    local playerPed = PlayerPedId()
+    playerPed = PlayerPedId()
+    local entity = playerPed
+    if IsPedInAnyVehicle(playerPed, false) then
+        entity = GetVehiclePedIsIn(playerPed, false)
+    end
+
     local camRot = GetGameplayCamRot(0)
     local forwardVector = RotationToDirection(camRot)
-
+    local rightVector = RotationToRightDirection(camRot)
     local speed = 1.0
-    if IsControlPressed(0, 21) then 
+
+    if IsControlPressed(0, 19) then 
+        speed = 0.5
+    elseif IsControlPressed(0, 21) then 
         speed = 2.5
     end
 
     if IsControlPressed(0, 32) then 
-        local pos = GetEntityCoords(playerPed) + forwardVector * speed
-        SetEntityCoordsNoOffset(playerPed, pos.x, pos.y, pos.z, true, true, true)
+        local pos = GetEntityCoords(entity) + forwardVector * speed
+        SetEntityCoordsNoOffset(entity, pos.x, pos.y, pos.z, true, true, true)
     end
 
     if IsControlPressed(0, 33) then 
-        local pos = GetEntityCoords(playerPed) - forwardVector * speed
-        SetEntityCoordsNoOffset(playerPed, pos.x, pos.y, pos.z, true, true, true)
+        local pos = GetEntityCoords(entity) - forwardVector * speed
+        SetEntityCoordsNoOffset(entity, pos.x, pos.y, pos.z, true, true, true)
     end
 
     if IsControlPressed(0, 44) then 
-        local pos = GetEntityCoords(playerPed) + vector3(0, 0, speed)
-        SetEntityCoordsNoOffset(playerPed, pos.x, pos.y, pos.z, true, true, true)
+        local pos = GetEntityCoords(entity) + vector3(0, 0, speed)
+        SetEntityCoordsNoOffset(entity, pos.x, pos.y, pos.z, true, true, true)
     end
 
     if IsControlPressed(0, 46) then 
-        local pos = GetEntityCoords(playerPed) + vector3(0, 0, -speed)
-        SetEntityCoordsNoOffset(playerPed, pos.x, pos.y, pos.z, true, true, true)
+        local pos = GetEntityCoords(entity) + vector3(0, 0, -speed)
+        SetEntityCoordsNoOffset(entity, pos.x, pos.y, pos.z, true, true, true)
+    end
+
+    if IsControlPressed(0, 30) then 
+        local pos = GetEntityCoords(entity) + rightVector * speed
+        SetEntityCoordsNoOffset(entity, pos.x, pos.y, pos.z, true, true, true)
+    end
+
+    if IsControlPressed(0, 34) then 
+        local pos = GetEntityCoords(entity) - rightVector * speed
+        SetEntityCoordsNoOffset(entity, pos.x, pos.y, pos.z, true, true, true)
     end
 
     local camRotZ = camRot.z
-    SetEntityHeading(playerPed, camRotZ)
+    SetEntityHeading(entity, camRotZ)
 end
 
 function RotationToDirection(rotation)
@@ -71,10 +123,19 @@ function RotationToDirection(rotation)
     return vector3(-math.sin(radZ) * num, math.cos(radZ) * num, math.sin(radX))
 end
 
+function RotationToRightDirection(rotation)
+    local radZ = math.rad(rotation.z)
+    return vector3(math.cos(radZ), math.sin(radZ), 0)
+end
+
+
 CreateThread(function()
     while true do
         Wait(0)
         if noclipActive then
+            if not IsPedInAnyVehicle(playerPed, false) and not IsEntityPlayingAnim(playerPed, "skydive@base", "free_idle", 3) then
+                startParachuteAnimation()
+            end
             handleNoclipMovement()
         end
     end
